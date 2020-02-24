4c3b256949bf65ac95585e1e6e13158355a6da42
Tutz26 <tutz_26@hotmail.com>
2020-02-24 10:15:52 -0700

    Created all main files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guess_The_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
